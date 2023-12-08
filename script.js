3c47f413b1b0317290c3ea9a447c438f6f317ea3
Mindy D Tran <mindytran98@gmail.com>
2023-12-08 13:38:55 -0500

    change how you declare robin and companions

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Character {
 }
 
 class Adventurer extends Character {
-    constructor (name, role, lv){
+    constructor (name, role = "Adventurer", lv = 1){
         super(name);
         this.role = role;
         this.level = lv;
@@ -32,10 +32,8 @@ class Companion extends Character{
 }
 const robin = new Adventurer("Robin");
 robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
+robin.companion = new Companion("Leo", "Cat");
+robin.companion.companion = new Companion("Frank", "Flea");
 robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 console.log(robin);
