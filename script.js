ec087845fc0a744cd3cc82c6728984eea5881b13
Mindy D Tran <mindytran98@gmail.com>
2023-12-08 13:26:58 -0500

    finish pt 2

--- script.js
+++ script.js
@@ -4,28 +4,19 @@ class Character {
         this.health = 100;
         this.inventory = [];
     }
-}
-
-// Part 1: Humble Beginnings
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"],
-    companion: {
-        name: "Leo",
-        type: "Cat",
-        subCompanion: {
-            name: "Frank",
-            type: "Flea"
-        }
-    },
     roll (mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
-        }
+    }
+
 }
 
-// print items in inventory
-adventurer.inventory.forEach(item => console.log(item));
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-adventurer.roll();
+console.log(robin);
