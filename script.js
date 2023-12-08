d40bca7ebae7be141c82993f3e5e19ae36459b63
Mindy D Tran <mindytran98@gmail.com>
2023-12-08 13:17:04 -0500

    part 1

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("owo");
+// Part 1: Humble Beginnings
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+}
+
+adventurer.inventory.forEach(item => console.log(item));
