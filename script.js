a442a378d15b74696b217c82723c36848dc2bb1f
Mindy D Tran <mindytran98@gmail.com>
2023-12-08 13:21:44 -0500

    part 1

--- script.js
+++ script.js
@@ -2,7 +2,22 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        subCompanion: {
+            name: "Frank",
+            type: "Flea"
+        }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+        }
 }
 
+// print items in inventory
 adventurer.inventory.forEach(item => console.log(item));
+
+adventurer.roll();
